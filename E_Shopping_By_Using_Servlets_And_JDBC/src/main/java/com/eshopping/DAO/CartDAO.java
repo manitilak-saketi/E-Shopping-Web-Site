63be90bb7726f9130d3f5f098e63debf6bc67e63
manitilak-saketi <158720497+manitilak-saketi@users.noreply.github.com>
2025-05-13 18:50:40 +0530

    Delete E_Shopping_By_Using_Servlets_And_JDBC directory

--- E_Shopping_By_Using_Servlets_And_JDBC/src/main/java/com/eshopping/DAO/CartDAO.java
+++
@@ -1,8 +0,0 @@
-package com.eshopping.DAO;
-
-import com.eshopping.model.Cart;
-
-public interface CartDAO {
-	int getCustomerCartCount(int customerId);
-	int insertCartDetails(Cart cart);
-}
--- E_Shopping_By_Using_Servlets_And_JDBC/src/main/java/com/eshopping/service/CustomerService.java
+++
@@ -1,8 +0,0 @@
-package com.eshopping.service;
-
-import com.eshopping.model.CustomerDetails;
-
-public interface CustomerService {
-	boolean userRegistration(CustomerDetails cd);
-	boolean forgetPassword(CustomerDetails cd);
-}
